004518858b89637e53a81337f8dee3ece31936f0
entradajuan <entradajuan@yahoo.es>
2021-07-12 16:59:44 +0000

    Tensorflow text

--- tokenizers/tf_text.py
+++ tokenizers/tf_text.py
@@ -1,5 +1,16 @@
+#!pip install tensorflow_text 
+
 import tensorflow as tf
 import tensorflow_text as tf_text
 
+text_input = ["Madrid is the capital of Spain.",
+              "I love to eat ice cream in Summer!!!",
+              "What's your name?"]
 
+def basic_preprocess(text_input, labels):
+  tokenizer = tf_text.WhitespaceTokenizer()
+  tokenized  = tokenizer.tokenize(text_input)
+  
+  return tokenized
 
+basic_preprocess(text_input, 0)
