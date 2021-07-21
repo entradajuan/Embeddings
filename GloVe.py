8d37e6d27af27d6ef9ae3573cc1586cc9da5c66d
entradajuan <entradajuan@yahoo.es>
2021-07-21 17:42:02 +0000

    Glove

--- GloVe.py
+++ GloVe.py
@@ -5,3 +5,37 @@ import pandas as pd
 
 tf.__version__
 
+
+
+tf.keras.backend.clear_session() #- for easy reset of notebook state
+
+# chck if GPU can be seen by TF
+tf.config.list_physical_devices('GPU')
+# only if you want to see how commands are executed
+#tf.debugging.set_log_device_placement(True)
+gpus = tf.config.experimental.list_physical_devices('GPU')
+if gpus:
+  # Restrict TensorFlow to only use the first GPU
+  try:
+    tf.config.experimental.set_memory_growth(gpus[0], True)
+    tf.config.experimental.set_visible_devices(gpus[0], 'GPU')
+    logical_gpus = tf.config.experimental.list_logical_devices('GPU')
+    print(len(gpus), "Physical GPUs,", len(logical_gpus), "Logical GPU")
+  except RuntimeError as e:
+    # Visible devices must be set before GPUs have been initialized
+    print(e)
+
+
+
+
+#--------------------------------------------------------------------
+# LOADING DATA
+
+imdb_train, ds_info = tfds.load(name="imdb_reviews", split="train", 
+                                with_info=True, as_supervised=True)
+imdb_test = tfds.load(name="imdb_reviews", split="test", 
+                      as_supervised=True)
+
+
+
+
