f812c4da586a7b2f3303d4a0d3bb9d04b63ee763
entradajuan <entradajuan@yahoo.es>
2021-09-11 18:12:43 +0000

    embedding example

--- embedding1.py
+++ embedding1.py
@@ -34,6 +34,73 @@ print(tokenizer.sequences_to_texts([tokenized_texts[0]]))
 text_sequences = tf.keras.preprocessing.sequence.pad_sequences(tokenized_texts)
 num_records = len(texts)
 max_seqlen = len(tokenized_texts[0])
+NUM_CLASSES = 2
+labels = tf.keras.utils.to_categorical(labels, num_classes=NUM_CLASSES)
+
+words_2_idx = tokenizer.word_index
+words_2_idx
+ids_2_words = {v:k for k, v in words_2_idx.items()}
+print(words_2_idx['house'])
+print(ids_2_words[words_2_idx['house']])
+
+# preparar dataset
+dataset = tf.data.Dataset.from_tensor_slices((texts, labels))
+dataset = dataset.shuffle(10000)
+test_size = num_records // 4
+val_size = (num_records - test_size) // 10
+test_dataset = dataset.take(test_size)
+val_dataset = dataset.skip(test_size).take(val_size)
+train_dataset = dataset.skip(test_size + val_size)
+
+BATCH_SIZE = 168
+test_dataset = test_dataset.batch(BATCH_SIZE, drop_remainder=True)
+val_dataset = val_dataset.batch(BATCH_SIZE, drop_remainder=True)
+train_dataset = train_dataset.batch(BATCH_SIZE, drop_remainder=True)
+
+# embedding
+EMBEDDING_MODEL = "glove-wiki-gigaword-300"
+
+def build_embedding_matrix(sequences, word2idx, embedding_dim, embedding_file):
+    if os.path.exists(embedding_file):
+        E = np.load(embedding_file)
+    else:
+        vocab_size = len(word2idx)
+        E = np.zeros((vocab_size, embedding_dim))
+        word_vectors = api.load(EMBEDDING_MODEL)
+        for word, idx in word2idx.items():
+            try:
+                E[idx] = word_vectors.word_vec(word)
+            except KeyError:   # word not in embedding
+                pass
+            # except IndexError: # UNKs are mapped to seq over VOCAB_SIZE as well as 1
+            #     pass
+        np.save(embedding_file, E)
+    return E
+
+
+EMBEDDING_DIM = 300
+DATA_DIR = "data"
+EMBEDDING_NUMPY_FILE = os.path.join(DATA_DIR, "E.npy")
+E = build_embedding_matrix(texts, words_2_idx, EMBEDDING_DIM,
+    EMBEDDING_NUMPY_FILE)
+print("Embedding matrix:", E.shape)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
