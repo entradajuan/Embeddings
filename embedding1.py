ec6ea60f246865fc2ca059715311dd70dd20a10b
entradajuan <entradajuan@yahoo.es>
2021-09-12 09:04:43 +0000

    embedding example

--- embedding1.py
+++ embedding1.py
@@ -85,6 +85,64 @@ EMBEDDING_NUMPY_FILE = os.path.join(DATA_DIR, "E.npy")
 E = build_embedding_matrix(text_sequences, words_2_idx, EMBEDDING_DIM, EMBEDDING_NUMPY_FILE)
 print("Embedding matrix:", E.shape)
 
+# Model definition
+
+
+class SpamClassifierModel(tf.keras.Model):
+    def __init__(self, vocab_sz, embed_sz, input_length,
+            num_filters, kernel_sz, output_sz, 
+            run_mode, embedding_weights, 
+            **kwargs):
+        super(SpamClassifierModel, self).__init__(**kwargs)
+        if run_mode == "scratch":
+            self.embedding = tf.keras.layers.Embedding(vocab_sz, 
+                embed_sz,
+                input_length=input_length,
+                trainable=True)
+        elif run_mode == "vectorizer":
+            self.embedding = tf.keras.layers.Embedding(vocab_sz, 
+                embed_sz,
+                input_length=input_length,
+                weights=[embedding_weights],
+                trainable=False)
+        else:
+            self.embedding = tf.keras.layers.Embedding(vocab_sz, 
+                embed_sz,
+                input_length=input_length,
+                weights=[embedding_weights],
+                trainable=True)
+        self.dropout = tf.keras.layers.SpatialDropout1D(0.2)
+        self.conv = tf.keras.layers.Conv1D(filters=num_filters,
+            kernel_size=kernel_sz,
+            activation="relu")
+        self.pool = tf.keras.layers.GlobalMaxPooling1D()
+        self.dense = tf.keras.layers.Dense(output_sz, 
+            activation="softmax"
+        )
+
+    def call(self, x):
+        x = self.embedding(x)
+        x = self.dropout(x)
+        x = self.conv(x)
+        x = self.pool(x)
+        x = self.dense(x)
+        return x
+
+run_mode = ""
+conv_num_filters = 256
+conv_kernel_size = 3
+vocab_size = len(words_2_idx) + 1
+model = SpamClassifierModel( vocab_size, EMBEDDING_DIM, max_seqlen, 
+    conv_num_filters, conv_kernel_size, NUM_CLASSES,
+    run_mode, E)
+    
+
+model.build(input_shape=(None, max_seqlen))
+model.summary()
+
+# compile and train
+model.compile(optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"])
+
 
 
 
