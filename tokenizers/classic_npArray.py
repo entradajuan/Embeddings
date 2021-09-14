9f4129809d016a356f0c879c6949df82ca344f3f
entradajuan <entradajuan@yahoo.es>
2021-09-14 17:33:11 +0000

    tokenizers

---
+++ tokenizers/classic_npArray.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+data = ["Madrid is the capital of Spain.",
+              "I love to eat ice cream in Summer!!!",
+              "What's your name?"]
+
+
+corpus = np.asarray(data)
+print(corpus)
+print(corpus.shape)
+
+
