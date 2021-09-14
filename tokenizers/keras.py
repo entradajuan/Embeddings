90d24a493270e2b299ba13a67330b17bf1d9fd39
entradajuan <entradajuan@yahoo.es>
2021-09-14 18:34:14 +0000

    tokenizers

--- tokenizers/keras.py
+++ tokenizers/keras.py
@@ -1,22 +1,29 @@
 import tensorflow as tf
 
-sentences = ["Madrid is the capital of Spain.",
-              "I love to eat ice cream in Summer!!!",
-              "What's your name?"]
+with open('kant.txt') as f:
+    sentences = f.readlines()
+
+#sentences = ["Madrid is the capital of Spain.",
+#              "I love to eat ice cream in Summer!!!",
+#              "What's your name?"]
 
 tokenizer = tf.keras.preprocessing.text.Tokenizer()
 tokenizer.fit_on_texts(sentences)
 sequences = tokenizer.texts_to_sequences(sentences)
-
-print(sequences)
 
 print("Count of characters:",tokenizer.word_counts)
 print("Length of text:",tokenizer.document_count)
 print("Character index",tokenizer.word_index)
 print("Frequency of characters:",tokenizer.word_docs)
 
-sequences = tf.keras.preprocessing.sequence.pad_sequences(sequences)
+sequences = tf.keras.preprocessing.sequence.pad_sequences(sequences, padding='post')
 
 print(sequences)
+print(type(sequences))
+print(sequences.shape)
+
+print("\n\n\n\n\n\n")
+print(sequences[1])
+print(tokenizer.sequences_to_texts([sequences[1]]))
 
 
